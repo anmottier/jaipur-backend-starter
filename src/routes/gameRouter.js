c8a5292a25f4c20b3cfcfcf4f3c4cdfb0e57fb7b
anmottier <antoine.49.mottier@gmail.com>
2022-11-07 19:30:46 +0100

    creation game

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -1,3 +1,9 @@
+import express from "express"
+
+import * as gameService from "../services/gameService"
+
+const router = express.Router()
+
 // Listen to POST /games
 router.post("/", function (req, res) {
     if (!req.body.name) {
@@ -6,3 +12,5 @@ router.post("/", function (req, res) {
     const newGame = gameService.createGame(req.body.name)
     res.status(201).json({ id: newGame.id, name: newGame.name })
   })
+
+  export default router
